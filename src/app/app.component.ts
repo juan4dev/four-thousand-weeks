c6ab66965e5a10869e71f0891ac6aed952578b75
Juan <72524993+jdlopez-dev@users.noreply.github.com>
2022-09-04 17:51:01 +0200

    feat: :sparkles: add calculation of weeks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'four-thousand-weeks';
+  title = '3';
+  totalWeeks: number = 0;
+
+  constructor() {}
+
+  updateWeeks(event: MatDatepickerInputEvent<Date>) {
+    this.totalWeeks = this.getWeeksDiff(event.value);
+  }
+
+  getWeeksDiff(date: Date | null) {
+    if (date) {
+      const msInWeek = 1000 * 60 * 60 * 24 * 7;
+      return Math.round(
+        Math.abs(date.getTime() - new Date().getTime()) / msInWeek
+      );
+    }
+    return 0;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
