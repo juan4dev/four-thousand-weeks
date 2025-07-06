3cd2bbac92b74b245abec4182ccf0153e26e78bc
juan4dev <72524993+juan4dev@users.noreply.github.com>
2025-07-06 17:30:42 +0200

    chore: upgrade Angular Material and CDK to version 15
    - Updated @angular/cdk from ^14.2.5 to ^15.2.9
    - Updated @angular/material from ^14.2.5 to ^15.2.9
    - Changed imports in material.module.ts to use legacy modules for form field and input

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDividerModule } from '@angular/material/divider';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
+import { MatLegacyFormFieldModule as MatFormFieldModule } from '@angular/material/legacy-form-field';
+import { MatLegacyInputModule as MatInputModule } from '@angular/material/legacy-input';
 
 @NgModule({
   exports: [
